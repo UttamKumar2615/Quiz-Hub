13f1fb26d6e4f77c713e4e8fe69c969ea30bf91f
unknown <ukpriyanshu111@gmail.com>
2022-02-04 17:04:54 +0530

    Interactive Quiz App

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a href="https://github.com/UttamKumar2615" style={{ cursor: "pointer" }}>
+        Uttam
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
